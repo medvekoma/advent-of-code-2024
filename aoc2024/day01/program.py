e0f91d80e9dce6800edb991338b71367f6487ec7
Attila Szucs <attila.szucs@gmail.com>
2024-12-01 18:17:20 +0100

    optimization

--- aoc2024/day01/program.py
+++ aoc2024/day01/program.py
@@ -3,19 +3,18 @@ from aoc2024.utils.reader import read_lines
 
 lines = read_lines()
 items = [tuple(map(int, line.strip().split())) for line in lines]
+list1, list2 = tuple(zip(*items))
 
 
 def part1():
-    l1, l2 = tuple(zip(*items))
-    l1 = sorted(l1)
-    l2 = sorted(l2)
+    l1 = sorted(list1)
+    l2 = sorted(list2)
 
     diff = [abs(x - y) for x, y in zip(l1, l2)]
     return sum(diff)
 
 
 def part2():
-    list1, list2 = list(zip(*items))
     counter = Counter(list2)
     occurence_map = {pair[0]: pair[1] for pair in counter.most_common()}
     scores = [x * occurence_map.get(x, 0) for x in list1]
