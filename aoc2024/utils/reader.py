a06c5a26b2b1843f3da828bfc4d4b04d7d6208f6
Attila Szucs <attila.szucs@gmail.com>
2024-12-02 08:07:56 +0100

    day02 (#1)

--- aoc2024/utils/reader.py
+++ aoc2024/utils/reader.py
@@ -5,4 +5,5 @@ def read_text(path: str = "input.txt") -> str:
 
 def read_lines(path: str = "input.txt") -> list[str]:
     with open(path, "r") as file:
-        return file.readlines()
+        lines = file.readlines()
+    return [line.strip() for line in lines]
