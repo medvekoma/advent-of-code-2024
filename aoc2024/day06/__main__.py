2d5f89b939f1d81958222d4841b1e4703143383b
Attila Szucs <attila.szucs@gmail.com>
2024-12-06 13:42:52 +0100

    Finalize day6

--- aoc2024/day06/__main__.py
+++ aoc2024/day06/__main__.py
@@ -2,13 +2,14 @@ from typing import Optional
 import numpy as np
 from aoc2024.utils.reader import read_lines
 
-lines = read_lines(is_test=True)
+lines = read_lines(is_test=False)
 
 array = [list(line) for line in lines]
 matrix1 = np.array(array)
 matrix = np.pad(matrix1, pad_width=1)
 position = tuple(np.argwhere(matrix == "^")[0])
-direction = (-1, 0)
+original_position = position
+orientation = (-1, 0)
 matrix[position] = "."
 
 
@@ -16,41 +17,78 @@ def add(a: tuple, b: tuple) -> tuple:
     return tuple(map(sum, zip(a, b)))
 
 
-def rotate(dir: tuple, clockwise: bool = True) -> tuple:
+def rotate(ori: tuple, clockwise: bool = True) -> tuple:
     if clockwise:
-        return dir[1], -dir[0]
-    return -dir[1], dir[0]
+        return ori[1], -ori[0]
+    return -ori[1], ori[0]
 
 
-def move_if_possible(pos: tuple, dir: tuple) -> Optional[tuple]:
-    new_pos = add(pos, dir)
-    if matrix[new_pos] == "#":
+def move_if_possible(mtx: np.ndarray, pos: tuple, ori: tuple) -> Optional[tuple]:
+    new_pos = add(pos, ori)
+    if mtx[new_pos] == "#":
         return None
     return new_pos
 
 
-def move(pos: tuple, dir: tuple) -> tuple[tuple, tuple]:
-    new_pos = move_if_possible(pos, dir)
+def move(mtx: np.ndarray, pos: tuple, ori: tuple) -> tuple[tuple, tuple]:
+    new_pos = move_if_possible(mtx, pos, ori)
     if new_pos:
-        return (new_pos, dir)
-    new_dir = rotate(dir)
-    return move(pos, new_dir)
+        return (new_pos, ori)
+    new_ori = rotate(ori)
+    return move(mtx, pos, new_ori)
 
 
 def part1():
-    pos, dir = position, direction
+    pos, ori = position, orientation
     positions = {position}
     while True:
-        pos, dir = move(pos, dir)
+        pos, ori = move(matrix, pos, ori)
         if matrix[pos] == "0":
             break
         positions.add(pos)
     return len(positions)
 
 
+def is_in_loop(
+    mtx: np.ndarray,
+    visited_pairs: set[tuple[tuple, tuple]],
+    pos: tuple,
+    ori: tuple,
+    next_pos: tuple,
+) -> bool:
+    if next_pos == original_position:
+        return False
+    new_mtx = mtx.copy()
+    new_mtx[next_pos] = "#"
+    pairs = visited_pairs.copy()
+    while True:
+        pos, ori = move(new_mtx, pos, ori)
+        if matrix[pos] == "0":
+            return False
+        pair = (pos, ori)
+        if pair in pairs:
+            return True
+        pairs.add(pair)
+
+
 def part2():
-    pass
+    pos, ori = position, orientation
+    positions = {position}
+    pairs = {(position, orientation)}
+    checked_positions = set()
+    blockers = set()
+    while True:
+        new_pos, new_ori = move(matrix, pos, ori)
+        if matrix[new_pos] == "0":
+            break
+        if new_pos not in checked_positions and is_in_loop(matrix, pairs, pos, ori, new_pos):
+            blockers.add(new_pos)
+        checked_positions.add(new_pos)
+        pos, ori = new_pos, new_ori
+        positions.add(pos)
+        pairs.add((pos, ori))
+    return (len(positions), len(blockers))
 
 
-print(f"part1: {part1()}")
+# print(f"part1: {part1()}")
 print(f"part2: {part2()}")
