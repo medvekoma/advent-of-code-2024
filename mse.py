d8bd674922ada2860f6b059e4037d9f757637eec
Attila Szucs <attila.szucs@gmail.com>
2024-12-08 08:40:13 +0100

    remove obsolete file

--- mse.py
+++
@@ -1,9 +0,0 @@
-text = input("Enter a number: ")
-num = int(text)
-for row in range(num):
-    for col in range(num):
-        if (row + col) % 2 == 0:
-            print("**", end="")
-        else:
-            print("  ", end="")
-    print()
