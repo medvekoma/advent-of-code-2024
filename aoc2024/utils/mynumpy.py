25f0b352875c5ee213bc9d50a6f6acb08f7723ef
Attila Szucs <attila.szucs@gmail.com>
2024-12-12 22:17:31 +0100

    part1

--- aoc2024/utils/mynumpy.py
+++ aoc2024/utils/mynumpy.py
@@ -1,7 +1,8 @@
-from typing import Any, Callable, TypeVar
+from typing import Any, Callable, Iterable, Optional, TypeVar
 import numpy as np
 
 T = TypeVar("T")
+type Cell = tuple[int, int]
 
 
 def identity_string(value: str) -> str:
@@ -18,11 +19,19 @@ class Matrix(np.ndarray):
         values = [[converter(ch) for ch in line] for line in lines]
         return Matrix(values)
 
-    def neighbors_of(self, cell: tuple[int, int]) -> list[tuple[int, int]]:
-        offsets = [(-1, 0), (1, 0), (0, -1), (0, 1)]
+    def neighbors_of(self, cell: tuple[int, int], offsets: Optional[list[Cell]] = None) -> list[Cell]:
+        offsets = offsets or [(-1, 0), (1, 0), (0, -1), (0, 1)]
         neighbors = [
             (cell[0] + r, cell[1] + c)
             for r, c in offsets
             if 0 <= cell[0] + r < self.shape[0] and 0 <= cell[1] + c < self.shape[1]
         ]
         return neighbors
+
+    def cells(self) -> Iterable[tuple[int, int]]:
+        yield from (
+            (r, c)
+            for r in range(self.shape[0])
+            for c in range(self.shape[1])
+            #
+        )
