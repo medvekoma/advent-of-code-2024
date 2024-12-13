989ba299e88d920cc5f52f6b0d05f62a0d890dd0
Attila Szucs <attila.szucs@gmail.com>
2024-12-13 10:02:41 +0100

    step1

--- aoc2024/day13/__main__.py
+++ aoc2024/day13/__main__.py
@@ -1,5 +1,5 @@
 import re
-from typing import Optional
+from typing import Iterable, Optional
 from aoc2024.utils.collections import split_by
 from aoc2024.utils.reader import read_lines
 
@@ -18,37 +18,28 @@ class Day13:
         ]
         for group in self.groups:
             res = self.solve_group(group)
-            print(res)
+            print(list(res))
 
-    def solve_group(self, group: Group) -> Optional[Cell]:
+    def solve_group(self, group: Group) -> Iterable[Cell]:
         print(group)
         (ax, ay), (bx, by), (wx, wy) = group
         divident = wy * ax - wx * ay
         divisor = by * ax - bx * ay
         if divisor == 0:
             if divident == 0:
+                # possible more solutions
                 asteps, amod = divmod(wx, ax)
-                bsteps, bmod = divmod(wx, bx)
-                if amod != 0 and bmod != 0:
-                    return None
-                if amod == 0 and bmod == 0:
-                    aprice = asteps * 3
-                    bprice = bsteps * 1
-                    if aprice <= bprice:
-                        return (asteps, 0)
-                    else:
-                        return (0, bsteps)
                 if amod == 0:
-                    return (asteps, 0)
-                return (0, bsteps)
-            return None
-        b, bmod = divmod(divident, divisor)
-        if bmod != 0:
-            return None
-        a, amod = divmod(wx - b * bx, ax)
-        if amod != 0:
-            return None
-        return a, b
+                    yield (asteps, 0)
+                bsteps, bmod = divmod(wx, bx)
+                if bmod == 0:
+                    yield (0, bsteps)
+        else:
+            b, bmod = divmod(divident, divisor)
+            if bmod == 0:
+                a, amod = divmod(wx - b * bx, ax)
+                if amod == 0:
+                    yield a, b
 
 
 Day13()
