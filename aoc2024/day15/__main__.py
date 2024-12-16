d727e5ac1c931bcc2680035a5d209625b9acd117
Attila Szucs <attila.szucs@gmail.com>
2024-12-16 17:09:57 +0100

    prep part2

--- aoc2024/day15/__main__.py
+++ aoc2024/day15/__main__.py
@@ -1,10 +1,10 @@
-from collections import Counter, defaultdict
+from collections import Counter, defaultdict, deque
 from math import prod
 from typing import Iterable, Optional
 
 import numpy as np
 from aoc2024.utils.benchmark import timer
-from aoc2024.utils.collections import group_list, parse_ints, split_by
+from aoc2024.utils.collections import group_list, parse_ints, split_by, split_by_func
 from aoc2024.utils.mynumpy import Matrix, Pos2D
 from aoc2024.utils.reader import read_lines
 
@@ -42,6 +42,28 @@ class Day:
             self.matrix[pos] = "."
             return nextpos
         return None
+
+    def extend_chunk(self, chunk: list[Pos2D]) -> list[Pos2D]:
+        if self.matrix[chunk[0]] == "]":
+            chunk = [chunk[0].add((0, -1))] + chunk
+        if self.matrix[chunk[-1]] == "[":
+            chunk = chunk + [chunk[-1].add((0, 1))]
+        return chunk
+
+    def get_steps_vert(self, positions: list[Pos2D], ori: str, prev_steps: int = 0) -> int:
+        next_positions = [pos.add(self.offsets[ori]) for pos in positions]
+        next_chars = [self.matrix[pos] for pos in next_positions]
+        if "#" in next_chars:
+            return 0
+        if set(next_chars) == {"."}:
+            return prev_steps + 1
+        next_chunks = list(split_by_func(next_positions, lambda pos: self.matrix[pos] == "."))
+        next_chunks = [self.extend_chunk(chunk) for chunk in next_chunks]
+        return min(
+            self.get_steps_vert(chunk, ori, prev_steps + 1)
+            for chunk in next_chunks
+            #
+        )
 
     def part1(self) -> None:
         pos = self.matrix.findall("@")[0]
