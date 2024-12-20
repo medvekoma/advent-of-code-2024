04e6927f46c59181ae9c00aa74e169c386e23406
Attila Szucs <attila.szucs@gmail.com>
2024-12-20 10:29:26 +0100

    finalize

--- aoc2024/day17/__main__.py
+++ aoc2024/day17/__main__.py
@@ -1,10 +1,7 @@
 from collections import defaultdict
-from math import prod
 from typing import Iterable, Optional
 
-import numpy as np
-from aoc2024.utils.benchmark import timer
-from aoc2024.utils.collections import group_list, parse_ints
+from aoc2024.utils.collections import parse_ints
 from aoc2024.utils.reader import read_lines
 
 IS_TEST = False
@@ -12,11 +9,11 @@ IS_TEST = False
 lines = read_lines(IS_TEST)
 
 
-class Day:
+class Computer:
     def __init__(self) -> None:
-        self.A = parse_ints(lines[0])[0]
-        self.B = parse_ints(lines[1])[0]
-        self.C = parse_ints(lines[2])[0]
+        self.a = parse_ints(lines[0])[0]
+        self.b = parse_ints(lines[1])[0]
+        self.c = parse_ints(lines[2])[0]
         self.program = parse_ints(lines[4])
         self.pointer = 0
         self.ops = {
@@ -31,49 +28,49 @@ class Day:
         }
 
     def reinit(self, a: int) -> None:
-        self.A = a
-        self.B = 0
-        self.C = 0
+        self.a = a
+        self.b = 0
+        self.c = 0
         self.pointer = 0
 
     def combo(self, operand: int) -> int:
         if operand in [0, 1, 2, 3]:
             return operand
         if operand == 4:
-            return self.A
+            return self.a
         if operand == 5:
-            return self.B
+            return self.b
         if operand == 6:
-            return self.C
+            return self.c
         raise ValueError(f"Invalid combo {operand}")
 
     def adv(self, operand: int) -> None:
         denominator = pow(2, self.combo(operand))
-        self.A = self.A // denominator
+        self.a = self.a // denominator
 
     def bxl(self, operand: int) -> None:
-        self.B = self.B ^ operand
+        self.b = self.b ^ operand
 
     def bst(self, operand: int) -> None:
-        self.B = self.combo(operand) % 8
+        self.b = self.combo(operand) % 8
 
     def jnz(self, operand: int) -> None:
-        if self.A != 0:
+        if self.a != 0:
             self.pointer = operand - 2  # -2 because we will add 2 in the processing loop
 
     def bxc(self, _: int) -> None:
-        self.B = self.B ^ self.C
+        self.b = self.b ^ self.c
 
     def out(self, operand: int) -> str:
         return str(self.combo(operand) % 8)
 
     def bdv(self, operand: int) -> None:
         denominator = pow(2, self.combo(operand))
-        self.B = self.A // denominator
+        self.b = self.a // denominator
 
     def cdv(self, operand: int) -> None:
         denominator = pow(2, self.combo(operand))
-        self.C = self.A // denominator
+        self.c = self.a // denominator
 
     def run(self) -> str:
         results = []
@@ -86,119 +83,113 @@ class Day:
         output = ",".join(res for res in results if res is not None)
         return output
 
-    def part1(self) -> None:
-        output = self.run()
-        print(f"Part 1: {output}")
 
-    def cycle(self, a: int) -> tuple[int, int]:
-        a1 = a // 8
-        # b1 = (a % 8) ^ (a // (2 ** ((a % 8) ^ 7)))
-        # c1 = a // (2 ** ((a % 8) ^ 7))
-        out = ((a % 8) ^ (a // (2 ** ((a % 8) ^ 7)))) % 8
-        return a1, out
+def part1() -> None:
+    computer = Computer()
+    output = computer.run()
+    print(f"Part 1: {output}")
 
-    def compiled(self, a: int) -> str:
-        outputs = []
-        while a != 0:
-            a, out = self.cycle(a)
-            outputs.append(str(out))
-        return ",".join(outputs)
 
-    def part1b(self) -> None:
-        output = self.compiled(self.A)
-        print(f"Part 1: {output}")
+def decoded_step(a: int) -> tuple[int, int]:
+    """
+    Decoded program, executes one iteration, returns A and output
+    """
+    a1 = a // 8
+    # b1 = (a % 8) ^ (a // (2 ** ((a % 8) ^ 7)))
+    # c1 = a // (2 ** ((a % 8) ^ 7))
+    out = ((a % 8) ^ (a // (2 ** ((a % 8) ^ 7)))) % 8
+    return a1, out
 
-    def digit_map(n: int) -> Iterable[dict[int, int]]:
-        for mod in range(8):
-            res: dict[int, int] = defaultdict(set)
-            last_bits = reversed("{:03b}".format(mod))
-            for i, bit in enumerate(last_bits):
-                res[i].add(int(bit))
-            first_bits = reversed("{:03b}".format(mod ^ n))
-            offset = 7 - mod
-            for i, bit in enumerate(first_bits):
-                res[offset + i].add(int(bit))
-            overlaps = {k for k, v in res.items() if len(v) > 1}
-            # print(">", mod, first_bits, last_bits, res)
-            if not overlaps:
-                yield {k: list(v)[0] for k, v in res.items()}
 
-    def shift_maps(maps: Iterable[dict[int, int]], digits: int) -> Iterable[dict[int, int]]:
-        for map in maps:
-            new_map = {k + digits: v for k, v in map.items()}
-            yield new_map
+def decoded_program(a: int) -> str:
+    """
+    Decoded program, executes all iterations, returns output
+    """
+    outputs = []
+    while a != 0:
+        a, out = decoded_step(a)
+        outputs.append(str(out))
+    return ",".join(outputs)
 
-    def merge_map(map1: dict[int, int], map2: dict[int, int]) -> Optional[dict[int, int]]:
-        all_keys = set(map1.keys()) | set(map2.keys())
-        result = {}
-        for key in all_keys:
-            v1 = map1.get(key, None)
-            v2 = map2.get(key, None)
-            if v1 is not None and v2 is not None and v1 != v2:
-                return None
-            result[key] = v1 if v1 is not None else v2
-        return result
 
-    def merge_maps(maps1: Iterable[dict[int, int]], maps2: Iterable[dict[int, int]]) -> Iterable[dict[int, int]]:
-        for map1 in maps1:
-            for map2 in maps2:
-                merged_map = Day.merge_map(map1, map2)
-                if merged_map:
-                    yield merged_map
+def digit_map(n: int) -> Iterable[dict[int, int]]:
+    """
+    Reversed step: for any output number, returns the possible bit values (position: value)
+    """
+    for mod in range(8):
+        res: dict[int, set[int]] = defaultdict(set)
+        last_bits = reversed(f"{mod:03b}")
+        for i, bit in enumerate(last_bits):
+            res[i].add(int(bit))
+        first_bits = reversed(f"{mod ^ n:03b}")
+        offset = 7 - mod
+        for i, bit in enumerate(first_bits):
+            res[offset + i].add(int(bit))
+        overlaps = {k for k, v in res.items() if len(v) > 1}
+        # print(">", mod, first_bits, last_bits, res)
+        if not overlaps:
+            yield {k: list(v)[0] for k, v in res.items()}
 
-    def get_value(postion_map: dict[int, int]) -> int:
-        # print(postion_map)
-        max_key = max(postion_map.keys())
-        buffer = ["0"] * (max_key + 1)
-        for k, v in postion_map.items():
-            buffer[max_key - k] = str(v)
-        binary = "".join(buffer)
-        print(binary)
-        return int(binary, 2)
 
-    def test() -> None:
-        day = Day()
-        maps = list(Day.digit_map(day.program[0]))
-        maps1 = list(Day.digit_map(day.program[1]))
-        maps1 = list(Day.shift_maps(maps1, 3))
-        maps = list(Day.merge_maps(maps, maps1))
-        maps2 = list(Day.digit_map(day.program[2]))
-        maps2 = list(Day.shift_maps(maps2, 6))
-        maps = list(Day.merge_maps(maps, maps2))
-        for m in maps:
-            # print(m)
-            v = Day.get_value(m)
-            # print(v)
-            day.reinit(v)
-            day.part1b()
+def shift_maps(maps: Iterable[dict[int, int]], digits: int) -> Iterable[dict[int, int]]:
+    for m in maps:
+        new_map = {k + digits: v for k, v in m.items()}
+        yield new_map
 
-    def show_maps(maps: Iterable[dict[int, int]]) -> None:
-        day = Day()
-        for m in maps:
-            # print(m)
-            v = Day.get_value(m)
-            # print(v)
-            day.reinit(v)
-            day.part1b()
 
-    def part2(self) -> None:
-        rolling_maps = None
-        for i, num in enumerate(self.program):
-            print(f"------- {i} -------")
-            current_maps = list(Day.digit_map(num))
-            if not rolling_maps:
-                rolling_maps = current_maps
-            else:
-                current_maps = list(Day.shift_maps(current_maps, i * 3))
-                rolling_maps = list(Day.merge_maps(rolling_maps, current_maps))
-            Day.show_maps(rolling_maps)
-        values = [Day.get_value(rolling_map) for rolling_map in rolling_maps]
-        print(f"Part 2: {min(values)}")
+def merge_map(map1: dict[int, int], map2: dict[int, int]) -> Optional[dict[int, int]]:
+    """
+    Combines two digit maps, returns the merged map if possible, None otherwise
+    """
+    all_keys = set(map1.keys()) | set(map2.keys())
+    result = {}
+    for key in all_keys:
+        v1 = map1.get(key, None)
+        v2 = map2.get(key, None)
+        if v1 is not None and v2 is not None and v1 != v2:
+            return None
+        result[key] = v1 or v2 or 0  # at least one of them is not None
+    return result
+
+
+def merge_maps(maps1: Iterable[dict[int, int]], maps2: Iterable[dict[int, int]]) -> Iterable[dict[int, int]]:
+    for map1 in maps1:
+        for map2 in maps2:
+            merged_map = merge_map(map1, map2)
+            if merged_map:
+                yield merged_map
+
+
+def get_value(postion_map: dict[int, int]) -> int:
+    """
+    Converts a binary digit map to an integer
+    """
+    max_key = max(postion_map.keys())
+    buffer = ["0"] * (max_key + 1)
+    for k, v in postion_map.items():
+        buffer[max_key - k] = str(v)
+    binary = "".join(buffer)
+    return int(binary, 2)
+
+
+def part2() -> None:
+    computer = Computer()
+    maps: Iterable[dict[int, int]] = []
+    for i, num in enumerate(computer.program):
+        current_maps = list(digit_map(num))
+        if not maps:
+            maps = current_maps
+        else:
+            current_maps = list(shift_maps(current_maps, i * 3))
+            maps = list(merge_maps(maps, current_maps))
+    values = [get_value(rolling_map) for rolling_map in maps]
+    print(f"Part 2: {min(values)}")
 
 
 def parts():
-    # Day().part1()
-    Day().part2()
+    part1()
+    part2()
 
 
-parts()
+if __name__ == "__main__":
+    parts()
---
+++ aoc2024/day17/decoder.py
@@ -0,0 +1,87 @@
+from aoc2024.utils.collections import parse_ints
+from aoc2024.utils.reader import read_lines
+
+IS_TEST = False
+
+lines = read_lines(IS_TEST)
+
+
+class LiteralComputer:
+    # pylint: disable=duplicate-code
+    def __init__(self) -> None:
+        self.a = "a"
+        self.b = "0"
+        self.c = "0"
+        self.program = parse_ints(lines[4])
+        self.pointer = 0
+        self.ops = {
+            0: self.adv,
+            6: self.bdv,
+            7: self.cdv,
+            1: self.bxl,
+            2: self.bst,
+            4: self.bxc,
+            3: self.jnz,
+            5: self.out,
+        }
+
+    def combo(self, operand: int) -> str:
+        if operand in [0, 1, 2, 3]:
+            return str(operand)
+        if operand == 4:
+            return f"({self.a})"
+        if operand == 5:
+            return f"({self.b})"
+        if operand == 6:
+            return f"({self.c})"
+        raise ValueError(f"Invalid combo {operand}")
+
+    def adv(self, operand: int) -> None:
+        self.a = f"({self.a}) // (2 ** {self.combo(operand)})"
+
+    def bxl(self, operand: int) -> None:
+        self.b = f"({self.b}) ^ {operand}"
+
+    def bst(self, operand: int) -> None:
+        self.b = f"({self.combo(operand)}) % 8"
+
+    def jnz(self, operand: int) -> None:
+        """Jump if not zero, doesn't affect registers"""
+
+    def bxc(self, _: int) -> None:
+        self.b = f"({self.b}) ^ ({self.c})"
+
+    def out(self, operand: int) -> None:
+        print(f"OUT: ({self.combo(operand)}) % 8")
+
+    def bdv(self, operand: int) -> None:
+        self.b = f"({self.a}) // (2 ** {self.combo(operand)})"
+
+    def cdv(self, operand: int) -> None:
+        self.c = f"({self.a}) // (2 ** {self.combo(operand)})"
+
+    def part2(self) -> None:
+        while self.pointer < len(self.program):
+            opcode, operand = self.program[self.pointer], self.program[self.pointer + 1]
+            operator = self.ops[opcode]
+            operator(operand)
+            self.pointer += 2
+        print(self.a)
+        print(self.b)
+        print(self.c)
+
+    # output_expression = ((a % 8) ^ (a // (2 ** ((a % 8) ^ 7)))) % 8
+    # a_mod_8 = {  #                     9876543210
+    #     0: (0 ^ (a // (2**7))) % 8,  # xxx....000
+    #     1: (1 ^ (a // (2**6))) % 8,  # .xxx...001
+    #     2: (2 ^ (a // (2**5))) % 8,  # ..xxx..010
+    #     3: (3 ^ (a // (2**4))) % 8,  # ...xxx.011
+    #     4: (4 ^ (a // (2**3))) % 8,  # ....xxx100
+    #     5: (5 ^ (a // (2**2))) % 8,  # .....xx101
+    #     6: (6 ^ (a // (2**1))) % 8,  # ......x110
+    #     7: (7 ^ (a // (2**0))) % 8,  # .......111
+    # }
+
+
+if __name__ == "__main__":
+    LiteralComputer().part2()
